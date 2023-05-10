3cbb2e1b1ccc06911fea09563821bb772ec151c4
cryptogus <wnelem1234@naver.com>
2023-05-10 22:59:34 +0900

    Initial code

---
+++ player.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+class Player {
+    
+    int total_;
+    
+    int acceleration_;
+    int sprint_;
+    int dribble_;
+    int shot_power_;
+    int free_kick_;
+    int pass_;
+    int curve_;
+
+  public:
+
+}
