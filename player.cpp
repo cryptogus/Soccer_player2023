d91332ddf4357fd049edf66e05641720b8f4bbfb
cryptogus <wnelem1234@naver.com>
2023-05-10 23:02:40 +0900

    Add constructor

--- player.cpp
+++ player.cpp
@@ -12,6 +12,11 @@ class Player {
     int pass_;
     int curve_;
 
+    static int level_; 
   public:
+    Player (int ac, int sp, int dri, int shot, int free, int pass, int cur)
+    {
+        
+    }
 
 }
